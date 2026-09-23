97e95e1f4a5317eb63fca742a4113f7d886b0038
Nagini <chakravartionlinecsc@gmail.com>
2026-09-23 22:37:27 +0530

    Add files via upload

---
+++ Day3.py
@@ -0,0 +1,13 @@
+#find thre given number is positive or negative or zero
+num = int(input("Enter the number:"))
+
+if num > 0 :
+    print("The number is Positive")
+
+elif num == 0:
+    print("The number is Zero")
+
+else:
+    print("The number is Negative")
+
+
